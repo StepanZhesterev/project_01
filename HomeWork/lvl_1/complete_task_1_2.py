f918d4ca2a4809f1433d8a5ae52fd10cf7250eaa
StepanZhesterev <137095756+StepanZhesterev@users.noreply.github.com>
2023-07-15 11:59:14 +0300

    попытка загрузить папку

---
+++ HomeWork/lvl_1/complete_task_1_2.py
@@ -0,0 +1,53 @@
+# Задача 1.2.
+
+# Пункт A. 
+# Приведем плейлист песен в виде списка списков
+# Список my_favorite_songs содержит список названий и длительности каждого трека
+# Выведите общее время звучания трех случайных песен в формате
+# Три песни звучат ХХХ минут
+
+
+my_favorite_songs = [
+    ['Waste a Moment', 3.03],
+    ['New Salvation', 4.02],
+    ['Staying\' Alive', 3.40],
+    ['Out of Touch', 3.03],
+    ['A Sorta Fairytale', 5.28],
+    ['Easy', 4.15],
+    ['Beautiful Day', 4.04],
+    ['Nowhere to Run', 2.58],
+    ['In This World', 4.02],
+]
+
+print("A.")
+total_my_favorite_songs = my_favorite_songs[0][1] + my_favorite_songs[2][1] + my_favorite_songs[4][1]
+print(f"Три песни звучат - {total_my_favorite_songs} минут")
+# Пункт B. 
+# Есть словарь песен 
+# Распечатайте общее время звучания трех случайных песен
+# Вывод: Три песни звучат ХХХ минут.
+
+my_favorite_songs_dict = {
+    'Waste a Moment': 3.03,
+    'New Salvation': 4.02,
+    'Staying\' Alive': 3.40,
+    'Out of Touch': 3.03,
+    'A Sorta Fairytale': 5.28,
+    'Easy': 4.15,
+    'Beautiful Day': 4.04,
+    'Nowhere to Run': 2.58,
+    'In This World': 4.02,
+}
+threesongs = (my_favorite_songs[6][1] + my_favorite_songs[4][1] + my_favorite_songs[2][1])
+print("B.")
+print(f" Три песни звучат {threesongs} минут")
+
+# Дополнительно для пунктов A и B
+# Пункт C.
+# Сгенерируйте случайные песни с помощью модуля random
+# import random
+
+# Дополнительно 
+# Пункт D.
+# Переведите минуты и секунды в формат времени. Используйте модуль datetime 
+
